61c0d953f896ac3f3a7b79be7f9d0d24dd43eaa9
igjerapic <i.a.gjerapic@student.rug.nl>
2025-11-21 14:16:56 +0100

    added addtional densities

--- stowed-workflows/N256_Density-sweep/workspace/N256_L32_D16/densities/init.py
+++ stowed-workflows/N256_Density-sweep/workspace/N256_L32_D16/densities/init.py
@@ -3,7 +3,7 @@ import os
 import signac
 import numpy as np
 
-project = signac.init_project() # initializes project in current working dir
+project = signac.get_project() # initializes project in current working dir
 
 # simulation parameters
 poly_length =  32
@@ -12,7 +12,7 @@ slab_thickness = 16
 
 # parameter space
 epsilon_PS = [0.5, 1.0, 2.0, 3.0]
-min_density = 0.84
+min_density = 0.74
 max_density = 1.2
 density_step = 0.02
 
@@ -21,8 +21,12 @@ densities = list(np.linspace(min_density, max_density, n_densities, dtype = floa
 slab_density = [round(rho, 2) for rho in densities]
 
 for rho in slab_density:
+    # skip previously initiated jobs
+    if rho >= 0.84 : continue
     for epsilon in epsilon_PS:
+        if epsilon != 3.0 : continue 
         sp = {'rho': rho , 'epLJ': epsilon, 'N': poly_length, 'D' : slab_thickness, 'T': 1.0, 'N_polymers': n_polymers, 
               'label': f'N{poly_length}_rho{rho}-epLJ{epsilon}'}
+
         job = project.open_job(sp).init()
  
